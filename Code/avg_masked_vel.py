15e27af50635fded48074dfd92f754b1cb791829
ajpetersen25 <whaticannotcreate@gmail.com>
2018-10-05 17:06:16 -0500

    Update avg_masked_vel.py

--- Code/avg_masked_vel.py
+++ Code/avg_masked_vel.py
@@ -2,3 +2,24 @@
 # save final averaged array as a separate .npy file
 
 from __future__ import division
+import numpy as np
+import numpy.ma as nma
+import load_masked_vel
+
+def average_masked(piv, mask):
+    """ 
+    Input: 
+      piv   --str:   path to .piv file
+      mask  --str:   path to .npy mask file
+    Output:
+      ave_mvel  --array: masked numpy array"""
+
+def main():
+  # calls average_masked and saves both average velocity field and mask for average field in separate
+  # .npy files (necessary until saving masked numpy arrays is implemented)
+  
+  
+ 
+
+if __name__ == "__main__":
+  main()
