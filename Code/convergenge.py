4c304c4c43857005ac9d0d92138677e0ffa6b2fe
ajpetersen25 <whaticannotcreate@gmail.com>
2018-10-05 17:19:13 -0500

    Update convergenge.py

--- Code/convergenge.py
+++ Code/convergenge.py
@@ -1,1 +1,6 @@
+# convergence of velocity profiles
+# for specified height(s) z, calculated and save an nxt array where n is the number of piv vectors in the
+# horizontal (x) direction, and t is the number of piv frames available. Each element is then the average
+# velocity for that x & z position up to that moment t in time.
+
 from __future__ import division
