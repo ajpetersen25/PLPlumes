2ea91bf1cfd5372bc435678b2c52995acc34166f
ajpetersen25 <whaticannotcreate@gmail.com>
2020-02-15 16:30:06 -0600

    plume_piv save name

--- PLPlumes/plume_processing/plume_piv.py
+++ PLPlumes/plume_processing/plume_piv.py
@@ -26,23 +26,22 @@ class Settings(object):
 
 def plume_piv(params):
     f1,f2,settings = params
-    frame_a = load_tif(f1)
-    frame_b = load_tif(f2)
+    frame_a = load_tif(f1).astype('int32')
+    frame_b = load_tif(f2).astype('int32')
     'first pass'
     x, y, u, v, sig2noise_ratio = first_pass(frame_a,frame_b,settings.window_sizes[0], settings.overlap[0],settings.iterations,
                                   correlation_method=settings.correlation_method, subpixel_method=settings.subpixel_method, 
                                   do_sig2noise=settings.extract_sig2noise,sig2noise_method=settings.sig2noise_method, 
                                   sig2noise_mask=settings.sig2noise_mask,)
-    
     mask=np.full_like(x,False)
     
     # Commented out sections should be uncommented for all Plumes except the dn45 View1 plumes
     #u, v, mask_m = local_median_val( u, v, u_threshold=settings.median_threshold, v_threshold=settings.median_threshold, 
     #                                           size=1)
-    #u,v, mask_s2n = sig2noise_val( u, v, sig2noise_ratio, threshold = settings.sig2noise_threshold)
+    u,v, mask_s2n = sig2noise_val( u, v, sig2noise_ratio, threshold = settings.sig2noise_threshold)
     u, v, mask_g = global_val( u, v, settings.MinMax_U_disp, settings.MinMax_V_disp)
-    #mask=mask+mask_g+mask_s2n
-    mask=mask+mask_g
+    mask=mask+mask_g+mask_s2n
+    #mask=mask+mask_g
     
     u, v = replace_outliers( u, v, method=settings.filter_method, max_iter=settings.max_filter_iteration, 
                                     kernel_size=settings.filter_kernel_size)
@@ -59,27 +58,25 @@ def plume_piv(params):
                     max_filter_iteration=settings.max_filter_iteration, filter_kernel_size=settings.filter_kernel_size,
                     interpolation_order=settings.interpolation_order)    
         
-    #u,v, mask_s2n = sig2noise_val( u, v, sig2noise_ratio, threshold = settings.sig2noise_threshold)
-    #mask=mask+mask_s2n
+    u,v, mask_s2n = sig2noise_val( u, v, sig2noise_ratio, threshold = settings.sig2noise_threshold)
+    mask=mask+mask_s2n
     u, v = replace_outliers( u, v, method=settings.filter_method, max_iter=settings.max_filter_iteration, 
                                     kernel_size=settings.filter_kernel_size)
-    'pixel/frame->pixel/sec'
-    #u=u/settings.dt
-    #v=v/settings.dt
-    save(x, y, u, v,sig2noise_ratio, mask ,os.path.join(settings.save_path,
-                                                        os.path.splitext(os.path.basename(frame_a))[0]+'.txt'), delimiter='\t')
-
+    print(u.shape)
+    print(os.path.join(settings.save_path,os.path.splitext(os.path.basename(frame_a))[0]+'.txt'))
+    save(x, y, u, v,sig2noise_ratio, mask ,os.path.join(settings.save_path, 
+                                                        os.path.splitext(os.path.basename(f1))[0]+'_piv.txt'), delimiter='\t')
+    
 def main():
     tic = time.time()
     # parse inputs
     parser = argparse.ArgumentParser(
                description='Program for parallel plume piv',
                formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('save_path',type=str,nargs=1,default='./',help='path to directory you want to save _bsub images')
+    parser.add_argument('save_path',type=str,nargs=1,help='path to directory you want to save piv files')
     parser.add_argument('cores',type=int,nargs=1,default=1,help='number of cores to use')
     parser.add_argument('files', nargs='+', help='Name of files as inputs')
     args = parser.parse_args()
-    
     img_files = args.files#glob.glob(args.path_to_image_files)   
     img_files.sort()
     # Settings
@@ -104,7 +101,7 @@ def main():
     settings.interpolation_order= 3
     #settings.dt = (1/600)
     #settings.scaling_factor
-    settings.save_path = args.save_path
+    settings.save_path = args.save_path[0]
     
     """ #Setting for View1 dn45 Plumes
     settings = Settings()
@@ -130,7 +127,7 @@ def main():
     #settings.scaling_factor
     settings.save_path = args.save_path"""
     
-    
+    print(settings.save_path)
     piv_imgs = []
     for i in range(1,len(img_files)):
         piv_imgs.append(img_files[i-1])
@@ -142,8 +139,7 @@ def main():
     objList = list(zip(piv_imgs1,piv_imgs2,
        repeat(settings,times=f_tot)))
 
-    pool = multiprocessing.Pool(processes=args.cores)
-    
+    pool = multiprocessing.Pool(processes=args.cores[0])
     pool.map(plume_piv,objList)
     
     print(('[FINISHED]: %f seconds elapsed' %(time.time()-tic)))
